4a0c630a133f2f90ae13ba42a8004822c7bb4f13
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-30 15:59:14 +0530

    [Manoj]Added_ConditionOrDaysReached

--- Program.cs
+++ Program.cs
@@ -15,11 +15,15 @@ namespace EmpWageCompute
             const int isPartTime = 2;
             const int MAX_WORKING_DAYS = 20;
             const int EMP_RATE_PER_HR = 20;
+            const int MAX_WORKING_HRS = 100;
+            int workingHrs = 0;
             int empHrs = 0;
             int empWage = 0;
             int totalWage = 0;
+            int day = 1;
             Random random = new Random();
-            for (int day = 1; day <= MAX_WORKING_DAYS; day++)
+            //for (int day = 1; day <= MAX_WORKING_DAYS; day++)
+            while (day <= MAX_WORKING_DAYS && workingHrs <= MAX_WORKING_HRS)
             {
                 int empInput = random.Next(0, 3);
                 switch (empInput)
@@ -36,10 +40,16 @@ namespace EmpWageCompute
                         break;
                 }
                 empWage = EMP_RATE_PER_HR * empHrs;
-                Console.WriteLine("Daily Wage:{0}", empWage);
-                Console.ReadLine();
+                Console.WriteLine("Daily Wage:{0} is:{1}", empWage);
+                totalWage += empWage;
+                day++;
+                workingHrs += empHrs;
+            }
+            Console.WriteLine("Total wage for {0} days is :{1} and workingHrs{2}",totalWage, workingHrs);
+            Console.ReadLine(); ;
+             
             }
         }
     }
-}
+
 
