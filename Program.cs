e9ffc4f21a69b2313866c7fe7a06ee58ea59a1f7
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-30 15:12:03 +0530

    [Manoj]Added_Attendence

--- Program.cs
+++ Program.cs
@@ -11,6 +11,20 @@ namespace EmpWageCompute
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage");
+            //UC1
+            int isPresent = 1;
+            Random random = new Random();
+            int empInput = random.Next(0, 2);
+
+            if (isPresent == empInput)
+            {
+                Console.WriteLine("employee present");
+
+            }
+            else
+            {
+                Console.WriteLine("employee absent");
+            }
         }
     }
 }
