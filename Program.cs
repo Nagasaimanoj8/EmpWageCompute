401549bce1b7fe7aac346371cc9608fd48a262e9
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-30 16:28:15 +0530

    [Manoj]Added_ConditionOrDaysReached

--- Program.cs
+++ Program.cs
@@ -21,32 +21,35 @@ namespace EmpWageCompute
             int totalempWage = 0;
             int hrs = 0;
             int day = 1;
-            Random random = new Random();//Random Class
+            Random random = new Random();  //Random Class
             //for (int day = 1; day <= MAX_WORKING_DAYS; day++)
             while (hrs <= MAX_WORKING_HRS && day <= MAX_WORKING_DAYS)
             {
-                int empInput = random.Next(0, 3);
-                switch (empInput)
+                int empInput = random.Next(0, 3);  //Random Generate 0,1,2
+                switch (empInput) //Switch case Statement
                 {
-                    case isFullTime:
+                    case isFullTime:      //Employee is FullTime=1
                         Console.WriteLine("employee is present");
                         empHrs = 8;
                         break;
-                    case isPartTime:
+                    case isPartTime:    //Employee is PartTime=2
                         empHrs = 4;
                         break;
                     default:
                         empHrs = 0;
                         break;
                 }
+                //Calaculate empwage
                 empWage = EMP_RATE_PER_HR * empHrs;
+                //Display emp Wage
                 Console.WriteLine("Daily Wage:{0} is:{1}", empWage);
                 totalempWage += empWage;
                 day++;
                 hrs += empHrs;
             }
-            Console.WriteLine();
-                Console.ReadLine();
+            Console.WriteLine("Total Employee Month Wage :" +totalempWage);
+            Console.WriteLine("Employee Wage For " + day + "days" + totalempWage);
+            Console.WriteLine("Working hours" +hrs);
             }
         }
     }
