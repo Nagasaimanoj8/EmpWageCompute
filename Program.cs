b372a47b8aa5bfd54ff5f871566988ef8113adb3
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-30 15:39:19 +0530

    [Manoj]Added_SwitchCase

--- Program.cs
+++ Program.cs
@@ -11,28 +11,28 @@ namespace EmpWageCompute
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage");
-            //UC1
+            const int isFullTime = 1;
+            const int isPartTime = 2; 
             int isPresent = 1;
             int empRatePerHr = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             int empInput = random.Next(0, 3);
+            switch (empInput)
+            {
+                case isFulTme:
+                    Console.WriteLine("employee is present");
+                    empHrs = 8;
+                    break;
+                case isPartTime:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
 
-            if (isPresent == empInput)
-            {
-                Console.WriteLine("employee present");
-                empHrs = 8;
-            }
-            else if(isPresent == empInput)
-            {
-                Console.WriteLine("employee is present");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("employee absent");
-                empWage = 0;
+            
             }
             empWage = empRatePerHr * empHrs;
             Console.WriteLine("Daily Wage:{0}",empWage);
