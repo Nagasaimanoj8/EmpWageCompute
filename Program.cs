a6751be6f0164bedb1d9679ac1f62e5754ae0f01
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-30 15:48:19 +0530

    [Manoj]Added_WagesForMonth

--- Program.cs
+++ Program.cs
@@ -12,31 +12,34 @@ namespace EmpWageCompute
         {
             Console.WriteLine("Welcome to the Employee Wage");
             const int isFullTime = 1;
-            const int isPartTime = 2; 
-            int isPresent = 1;
-            int empRatePerHr = 20;
+            const int isPartTime = 2;
+            const int MAX_WORKING_DAYS = 20;
+            const int EMP_RATE_PER_HR = 20;
             int empHrs = 0;
             int empWage = 0;
+            int totalWage = 0;
             Random random = new Random();
-            int empInput = random.Next(0, 3);
-            switch (empInput)
+            for (int day = 1; day <= MAX_WORKING_DAYS; day++)
             {
-                case isFulTme:
-                    Console.WriteLine("employee is present");
-                    empHrs = 8;
-                    break;
-                case isPartTime:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-
-            
+                int empInput = random.Next(0, 3);
+                switch (empInput)
+                {
+                    case isFullTime:
+                        Console.WriteLine("employee is present");
+                        empHrs = 8;
+                        break;
+                    case isPartTime:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = EMP_RATE_PER_HR * empHrs;
+                Console.WriteLine("Daily Wage:{0}", empWage);
+                Console.ReadLine();
             }
-            empWage = empRatePerHr * empHrs;
-            Console.WriteLine("Daily Wage:{0}",empWage);
-            Console.ReadLine();
         }
     }
 }
+
