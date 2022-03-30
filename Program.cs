92aa77cd977bc223795230d1938451c375cb55d8
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-30 15:23:26 +0530

    [Manoj]Added_EmployeeDailyWage :wq

--- Program.cs
+++ Program.cs
@@ -13,18 +13,24 @@ namespace EmpWageCompute
             Console.WriteLine("Welcome to the Employee Wage");
             //UC1
             int isPresent = 1;
+            int empRatePerHr = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int empInput = random.Next(0, 2);
 
             if (isPresent == empInput)
             {
                 Console.WriteLine("employee present");
-
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("employee absent");
             }
+            empWage = empRatePerHr * empHrs;
+            Console.WriteLine("Daily Wage:{0}",empWage);
+            Console.ReadLine();
         }
     }
 }
